97cd5ded7efdaa01a1e14b65e948d073708e6d6a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-27 16:04:01 +0530

    Update settings.py

--- meeting_planner/meeting_planner/settings.py
+++ meeting_planner/meeting_planner/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website',  # Add this line to the list of installed apps
-    # 'website.apps.WebsiteConfig',  # Add this line to the list of installed apps
+    'website.apps.WebsiteConfig',  # Add this line to the list of installed apps
 ]
 
 MIDDLEWARE = [
