3638dadd8e5864b8cd340816dbce21cd7ce81eed
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-27 16:11:13 +0530

    About Page

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 
 def welcome(request):
     return HttpResponse("Welcome to the Meeting Planner!")
+
+
+def about(request):
+    return HttpResponse("I'm  Swamy The Learner !!")
