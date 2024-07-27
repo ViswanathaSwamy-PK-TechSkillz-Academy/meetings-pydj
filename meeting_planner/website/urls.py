89c707500c549a09b17528692d84de4c09af7b83
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-27 16:14:09 +0530

    Changes

---
+++ meeting_planner/website/urls.py
@@ -0,0 +1,8 @@
+# website/urls.py
+from django.urls import path
+from .views import welcome, about
+
+urlpatterns = [
+    path('', welcome, name='welcome'),
+    path('about/', about, name='about'),
+]
