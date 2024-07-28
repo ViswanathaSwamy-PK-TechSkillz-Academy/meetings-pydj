8f2423d0b2a98165a2088336e73b117e0b56e7ad
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-28 17:06:03 +0530

    Changes

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from meetings.models import Meeting
+
+
 # Create your views here.
-
-
 def welcome(request):
-    return render(request, "website/welcome.html")
+    return render(request, "website/welcome.html",
+                  {"meetings": Meeting.objects.all()})
 
 
 def about(request):
