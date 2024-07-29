8a48514fec4a5d3ca1b11ea584359eb5b2085b9c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-29 08:59:51 +0530

    Swamy/28jul work (#2)
    * Copied Templates
    
    * Templates
    
    * HOme View
    
    * Changes
    
    * Update style.css
    
    * Detail View
    
    * Changes
    
    * Update style.css
    
    * Changes
    
    * Update style.css
    
    * Rooms

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from meetings.models import Meeting
+
+
 # Create your views here.
-
-
 def welcome(request):
-    return HttpResponse("Welcome to the Meeting Planner!")
+    return render(request, "website/welcome.html",
+                  {"meetings": Meeting.objects.all()})
 
 
 def about(request):
     return HttpResponse("I'm  Swamy The Learner !!")
+
+
+# def welcome(request):
+#     return HttpResponse("Welcome to the Meeting Planner!")
